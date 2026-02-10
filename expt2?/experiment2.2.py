17f14b7dd37b07becc76b12938739590f34f28ab
jshruti2506-hue <jshruti2506@gmail.com>
2026-02-10 13:56:54 +0530

    Add files via upload

---
+++ expt2?/experiment2.2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 10 12:52:33 2026
+
+@author: shruti priti 
+"""
+n=int (input ( "Enter number: " ))
+fact=1
+for i in range(1,n+1):
+    fact = fact*i
+    print ( "factorial",fact)
+    
