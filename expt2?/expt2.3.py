1b0f9fd3b6b00418339483e828e7f946104e2327
jshruti2506-hue <jshruti2506@gmail.com>
2026-02-10 13:57:31 +0530

    Add files via upload

---
+++ expt2?/expt2.3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 10 13:01:35 2026
+
+@author: shruti priti
+"""
+speed=int (input ( "enter vehical speed (km/h ) :"))
+if speed >60 :
+    print ( "warninig: overspeeding is an offense.fine charged") 
+else :
+    print ( "speed is within limit.no fine.")
