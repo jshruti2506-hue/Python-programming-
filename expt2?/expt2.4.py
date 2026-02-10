85bb02bee16b3fbe162f7ea4f807474e38498bc3
jshruti2506-hue <jshruti2506@gmail.com>
2026-02-10 14:00:25 +0530

    Add files via upload

---
+++ expt2?/expt2.4.py
@@ -0,0 +1,12 @@
+# check eligibility age criteria
+"""
+Created on Tue Feb 10 13:14:59 2026
+
+@author: shruti,priti
+"""
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible for movie entry.")
+else:
+    print("You are not eligible for movie entry.")
