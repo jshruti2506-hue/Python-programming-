80c953c6d2cba056ecf9835936c3e49f0e0384a4
jshruti2506-hue <jshruti2506@gmail.com>
2026-03-10 13:50:58 +0530

    Add files via upload

---
+++ exp4/exp4 activity1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:36:56 2026
+
+@author: User
+"""
+
+marks = [78, 85, 90, 67, 88]
+
+average = sum(marks) / len(marks)
+topper = max(marks)
+
+print("Average Marks:", average)
+print("Topper Marks:", topper)
