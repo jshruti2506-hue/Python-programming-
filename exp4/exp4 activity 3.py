8dd8f14d1a1b75d0dfc80bdeeb28bbb65ae8a048
jshruti2506-hue <jshruti2506@gmail.com>
2026-03-10 13:52:55 +0530

    Add files via upload

---
+++ exp4/exp4 activity 3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:41:09 2026
+
+@author: User
+"""
+
+feedback = "The service was excellent"
+
+# Using slicing to reverse
+reversed_feedback = feedback[::-1]
+
+print(f"Original: {feedback}")
+print(f"Reversed: {reversed_feedback}")
