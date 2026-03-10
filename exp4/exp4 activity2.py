3890032d8e55a920bab6f28c4e2ccded6dce47d8
jshruti2506-hue <jshruti2506@gmail.com>
2026-03-10 13:52:16 +0530

    Add files via upload

---
+++ exp4/exp4 activity2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:39:16 2026
+
+@author: User
+"""
+
+prices = [499, 1200, 150, 2500]
+
+total_bill = sum(prices)
+
+print(f"Total Bill Amount: ₹{total_bill}")
