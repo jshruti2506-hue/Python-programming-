801b06577555451a2dd42f4e27be4582cdea15ac
jshruti2506-hue <jshruti2506@gmail.com>
2026-03-17 13:09:48 +0530

    Add files via upload

---
+++ exp5 activity 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:47:40 2026
+
+@author: shruti jadhav 
+"""
+
+classA = {1, 2, 3, 4, 5}
+classB = {4, 5, 6, 7}
+
+# Common students
+common = classA.intersection(classB)
+
+print(common)
