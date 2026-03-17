56e1be00ff1e701dc39d19760bd29e7268c0e00d
jshruti2506-hue <jshruti2506@gmail.com>
2026-03-17 13:11:24 +0530

    Add files via upload

---
+++ exp5/exp5 activity 2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:57:44 2026
+
+@author: shruti jadhav
+
+"""
+account1 = {"shruti", "sneha", "amit", "abhi"}
+account2 = {"shruti", "abhi", "karan"}
+
+common_followers = account1 & account2
+
+print(common_followers)
+
