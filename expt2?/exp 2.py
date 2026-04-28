18d1393bb155df44807628ff8a6f012b7a7e3ac5
jshruti2506-hue <jshruti2506@gmail.com>
2026-04-28 16:54:08 +0530

    Add files via upload

---
+++ expt2?/exp 2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 16:38:27 2026
+
+@author: shruti jadhav
+"""
+n = int(input("Enter number:"))
+fact = 1
+for i in range(1, n + 1):
+fact = fact * i
+print("Factorial:", fact)
