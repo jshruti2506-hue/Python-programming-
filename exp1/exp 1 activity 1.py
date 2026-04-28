d9867527293ba6d83d7ca105079311523f719dd3
jshruti2506-hue <jshruti2506@gmail.com>
2026-04-28 16:21:31 +0530

    Add files via upload

---
+++ exp1/exp 1 activity 1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 16:16:32 2026
+
+@author: shruti jadhav
+"""
+
+"""
+Created on Tue Apr 28 16:15:40 2026
+
+@author: shruti jadhav
+"""
+# Program to display personal details
+name = "Your Name"
+address = "Your Full Address, City, Pincode"
+contact_number = "Your Contact Number"
+
+print("--- Personal Details ---")
+print(f"Name: {name}")
+print(f"Address: {address}")
+print(f"Contact Number: {contact_number}")
+
